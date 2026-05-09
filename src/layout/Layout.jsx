317fb4b92ba0985ca72e3fc67d8be445eafbcfb1
teran210c <a242729@uach.mx>
2026-05-10 00:09:07 +0100

    Fix: set up the Topbar.jsx file appereance

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,21 @@
 import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
 
 export default function Layout () {
     return (
-        <div>
+        <div 
+            className="d-flex flex-grow-1"
+            style={{ backgroundColor: "#F1F5F9" }}
+        >
             <div style={{width: "220px"}}>
                 <Sidebar />
             </div>
+            <div 
+                className="flex-grow-1"
+            >
+                <Topbar />
+            </div>
+            
         
         </div>
     )
---
+++ src/layout/Topbar.jsx
@@ -0,0 +1,13 @@
+export default function Topbar() {
+    return (
+        <div 
+            className="d-flex align-items-center px-4 border-bottom"
+            style={{ 
+                backgroundColor: "#F8FAFC",
+                height: "70px"
+             }}
+        >
+            Topbar
+        </div>
+    )
+}
