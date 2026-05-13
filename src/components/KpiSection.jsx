be6e398c4573c3786e411fc00b22389cdbc361a6
teran210c <162738757+teran210c@users.noreply.github.com>
2026-05-13 01:42:41 +0000

    feat: created new component

---
+++ src/components/KpiSection.jsx
@@ -0,0 +1,22 @@
+import KpiCard from "../components/KpiCard";
+
+export default function KpiSection({ kpiCards }) {
+
+    return (
+        <div
+            className="d-flex pt-4 gap-5 justify-content-center"
+        >
+            {kpiCards.map((kpiCard, index) => {
+                return (
+                    <KpiCard
+                        key={index}
+                        title={kpiCard.title}
+                        value={kpiCard.value}
+                        
+                    />
+                )
+            })}
+            
+        </div>
+    )
+}
