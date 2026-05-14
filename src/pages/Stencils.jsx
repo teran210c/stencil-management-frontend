dcafe17ff972ab81e9c868f5ad59eed4c77e5c88
teran210c <162738757+teran210c@users.noreply.github.com>
2026-05-14 02:11:07 +0000

    Feat: created Stencils.jsx and added searchbar

--- src/pages/Stencils.jsx
+++ src/pages/Stencils.jsx
@@ -1,7 +1,27 @@
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 export default function Stencils() {
     return (
-        <div>
-            Stencils
-        </div>
+        <div
+            className="p-4"
+        >
+            <h1>
+                Manage and monitor stencil validation
+            </h1>
+            <form style={{ maxWidth: '350px' }}>
+                <div className="input-group input-group-sm mb-3">
+                    <span className="input-group-text" id="basic-addon1">
+                        <i className="bi bi-search"></i>
+                    </span>
+                    <input
+                        type="search"
+                        placeholder="Search Stencil"
+                        // value={searchTerm}
+                        // onChange={handleChange}
+                        className="form-control"
+                    />
+                </div>
+            </form >
+        </div >
     )
 }
