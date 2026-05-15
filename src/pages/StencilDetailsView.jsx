188522a3ac9f3e0f0ee095c01c93165e05b33659
teran210c <162738757+teran210c@users.noreply.github.com>
2026-05-15 23:23:20 +0000

    Feat:add StencilValidationView and set up fetch function for stencil by id

--- src/pages/StencilDetailsView.jsx
+++ src/pages/StencilDetailsView.jsx
@@ -28,8 +28,6 @@ export default function StencilDetailsView() {
         
     }, [])
 
-    console.log(stencil)
-
     return (
         <div>
             <div>
--- src/pages/StencilValidationView.jsx
+++ src/pages/StencilValidationView.jsx
@@ -1,10 +1,43 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export default function StencilValidationView() {
     const {stencilId} = useParams()
+    const [stencil, setStencil] = useState({})
+
+    useEffect(() => {
+
+        const fetchStencil = async () => {
+
+            const res = await fetch(`http://localhost:8080/stencils/${stencilId}`)
+
+            const data = await res.json()
+
+            setStencil(data)
+
+        }
+
+        fetchStencil()
+
+    }, [])
+
+    console.log(stencil)
+
     return (
         <div>
             ValidationView
+            <div>
+                {stencil.number}
+                {stencil.status}
+            </div>
+            <div>
+                <div>
+                    checklist
+                </div>
+                <div>
+                    Validation Summary
+                </div>
+            </div>
         </div>
     )
 }
