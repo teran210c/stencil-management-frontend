adb8a53e8858cbcc2bcac0713524552009038bad
teran210c <a242729@uach.mx>
2026-05-15 14:02:40 -0600

    feat: add fetch fuction, fetched info and added buttons

--- src/pages/StencilDetailsView.jsx
+++ src/pages/StencilDetailsView.jsx
@@ -1,18 +1,49 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
 export default function StencilDetailsView() {
     const { stencilId } = useParams()
+    const [stencil, setStencil] = useState({})
 
-    console.log(stencilId)
+    useEffect(() => {
+        const fetchStencil = async () => {
+
+            try {
+            
+            const res = await fetch(`http://localhost:8080/stencils/${stencilId}`)
+
+            const data = await res.json()
+
+            setStencil(data)
+
+        } catch (error) {
+
+            console.error("Error fetching stencil", error)
+            
+        }
+
+        }
+
+        fetchStencil()
+        
+    }, [])
+
+    console.log(stencil)
 
     return (
         <div>
             <div>
-                StencilHeader
+                {stencil.number}
             </div>
             <div>
                 <div>
                     StencilInfoCard
+                    <ul>
+                        <li>{stencil.vendor}</li>
+                        <li>Location</li>
+                        <li>Expiration Date</li>
+                        <li>Cycle Days</li>
+                    </ul>                                      
                 </div>
                 <div>
                     ValidationHistory
@@ -23,7 +54,9 @@ export default function StencilDetailsView() {
                     CurrentValidation
                 </div>
                 <div>
-                    StencilActions
+                    <button type="button" className="btn btn-success">Start Validation</button>
+                    <button type="button" className="btn btn-secondary">Edit Stencil</button>
+                    <button type="button" className="btn btn-secondary">Disable Stencil</button>
                 </div>
             </div>
         </div>
